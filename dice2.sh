1c3a0788882bf5acad13a8b9733287985fefaf3f
hello35 <rktireleluck21@gmail.com>
2022-06-14 22:22:40 -0700

    Added: Programming Exc.1

---
+++ dice2.sh
@@ -0,0 +1,21 @@
+echo "Random Number was: " $dice
+
+if((  dice == 1  ))
+then
+        echo "One"
+elif((  dice == 2  ))
+then
+         echo "Two"
+elif((  dice == 3  ))
+then
+          echo "Three"
+elif((  dice == 4  ))
+then
+           echo "Four"
+elif((  dice == 5  ))
+then
+           echo "Five"
+else
+            echo "Six"
+fi
+
