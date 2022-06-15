b52f4678c2aa596fa4dcdd3776072213ea00273f
hello35 <rktireleluck21@gmail.com>
2022-06-14 22:16:07 -0700

    Added: Programming Exc.1

---
+++ dice.sh
@@ -0,0 +1,17 @@
+dice_num=$(( 1 + RANDOM%6  ))
+
+case $dice_num in
+                1) echo "One"
+                        ;;
+                2) echo "Two"
+                        ;;
+                3) echo "Three"
+                        ;;
+                4) echo "Four"
+                        ;;
+                5) echo "Five"
+                        ;;
+                *) echo "Six"
+                        ;;
+esac
+
