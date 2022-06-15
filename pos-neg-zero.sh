c8d2cdc2fd519de76353409815a2e7fa51db16e8
hello35 <rktireleluck21@gmail.com>
2022-06-14 22:04:02 -0700

    Added: Programming Exc.1

---
+++ pos-neg-zero.sh
@@ -0,0 +1,14 @@
+read -p "Enter the number: " num
+
+if(( num > 0 ))
+then
+       echo "Positive Number"
+elif(( num < 0 ))
+then
+       echo "Negative Number"
+else
+       echo "It is Zero"
+fi
+
+
+
